f01a03b1214f23eb2127580b713aeb17d9752604
al-ek-sa <aleksandra.lishik@gmail.com>
2026-09-22 19:31:51 +0300

    feat: Theory for the second and third levels has been added, and the CSS file has been edited

--- front/js/nevigation.js
+++ front/js/nevigation.js
@@ -2,10 +2,18 @@ export function initNavigation(){
   const lv1 = document.getElementById('lv1');
   const history = document.getElementById('history--1lv');
   const exit = document.getElementById('exit--1lv');
-  const lv1Success = document.getElementById('understand-true');
+  const lv1Success = document.getElementById('success--exit--lv1');
   const lv1Failure = document.getElementById('failure-lv1');
   const modalLv1= document.getElementById('false');
   const tableLv1 = document.getElementById('but-table-1lv');
+  const lv1toLv2 = document.getElementById('success--next--lv1');
+
+  if(lv1toLv2) {
+    lv1toLv2.addEventListener('click', event => {
+      event.preventDefault();
+      window.location.href = 'lv2.html';
+    });
+  }
 
   if(tableLv1){
     tableLv1.addEventListener('click', (event) => {
